f335900e9d89836276ea14552f584ed8459aaf18
Anik Mandal <mandal.anik10@gmail.com>
2024-01-23 13:09:02 +0530

    final check: py-my_project

---
+++ py-my_project/linearfit_sklearn_linearReg.py
@@ -0,0 +1,28 @@
+'''
+Linear fitting using scikit-learn LinearRegression
+
+Author : Anik Mandal
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Creating Sample Experimental Data:
+x_data = np.array([[1], [2], [3], [4], [5], [6]])
+y_data = 2 * x_data + 3 + 0.5 * np.random.randn(6, 1)
+
+mdl = LinearRegression().fit(x_data, y_data)
+
+m = mdl.coef_
+c = mdl.intercept_
+print(m, c)
+xx = np.linspace(1, 6, 100)
+yy = []
+for i in range(len(xx)):
+    yy.append(m[0][0] * xx[i] + c[0])
+
+plt.plot(x_data, y_data, '+r', xx, yy, 'c')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
